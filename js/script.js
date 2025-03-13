4f4748cb3397a8815437d7879b3cb1a6b89ee6b3
Lelonio <emanuelemaio18@gmail.com>
2025-03-13 18:00:49 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,14 +22,7 @@ $(document).ready(function() {
       
 })
 
-$(".to-exclusive a").click(function(event) {
-  event.preventDefault();
-  var sectionTop = $('#btnMenu').offset().top + 15;
-  $('html,body').animate({
-    scrollTop: sectionTop },
-    'slow');
-  
-})
+
 
 $(".to-who a").click(function(event) {
   event.preventDefault();
@@ -47,14 +40,7 @@ $(".to-info a").click(function(event) {
     'slow');
   
 })
-$(".to-prenotazione a").click(function(event) {
-  event.preventDefault();
-  var sectionTop = $('.prenotazione').offset().top;
-  $('html,body').animate({
-    scrollTop: sectionTop },
-    'slow');
-  
-})
+
 
 $(".to-faq a").click(function(event) {
   event.preventDefault();
@@ -69,101 +55,8 @@ $('.faq li .question').click(function () {
   $(this).parent().toggleClass('active');
 });
 
-$('.experience li .experience-btn').click(function (event) {
-  event.preventDefault();
-  $(this).parent().toggleClass('active');
-  var sectionTop = $('.exclusive-menu').offset().top ;
-  $('html,body').animate({
-    scrollTop: sectionTop },
-    'slow');
-});
-
-
-$('#prenota').click(function (e) {
-
-  e.preventDefault();
-  if ($('#prenota-checkbox').is(':checked')) { 
-  Swal.fire({
-    title: 'Confermare Prenotazione?',
-    text: "Se non sei sicuro, controlla i dati",
-    icon: 'question',
-    showCancelButton: true,
-    confirmButtonColor: '#be6440',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Prenota',
-    cancelButtonText: 'Annulla',
-    showLoaderOnConfirm: true,
-  }).then((result) => {
-    if (result.isConfirmed) {
-      $.ajax({
-        type: 'POST', 
-        url: 'checkPrenotazione.php', 
-        data: $('#prenotaForm').serialize(), 
-        success: function (response) {
-          $("#result").append(response); 
-        }
-      });
-    }
-  })
-}
-
-else{
-  Swal.fire({
-    title:'Il consenso è importante',
-    text:'Per prenotare devi prima accettare la policy sui dati personali',
-    icon:'error',
-    confirmButtonColor: '#be6440'
-})
-}
-
-
-
-});
-
-$('#prenota-experience').click(function (e) {
-
-  e.preventDefault(); 
-
-  if ($('#experience-checkbox').is(':checked')) { 
-  Swal.fire({
-    title: 'Confermare Experience?',
-    text: "Se non sei sicuro, controlla i dati",
-    icon: 'question',
-    showCancelButton: true,
-    confirmButtonColor: '#be6440',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Regala',
-    cancelButtonText: 'Annulla',
-  }).then((result) => {
-    if (result.isConfirmed) {
-      $.ajax({
-        type: 'POST', 
-        url: 'checkExperience.php', 
-        data: $('#experienceForm').serialize(), 
-        success: function (response) {
-          $("#result").append(response); 
-        }
-      });
-    }
-  })
-  }  
-
-  else{
-
-    Swal.fire({
-      title:'Il consenso è importante',
-      text:'Per prenotare devi prima accettare la policy sui dati personali',
-      icon:'error',
-      confirmButtonColor: '#be6440',
-  })
-
-  }
-
-});
-
-
   AOS.init({      
-        duration: 1000,
+        duration: 500,
     easing: 'ease'
   });
    
